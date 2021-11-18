b7a5300a999403770840ffbccbdf108cdb2cd868
jzxr <jasminezxr@gmail.com>
2021-11-18 12:33:36 +0800

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSC2011_LAB11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
